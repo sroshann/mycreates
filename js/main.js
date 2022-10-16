7bdd357cd0f1944489220e72cb3e394a6d398e31
Shamil Roshan <shamilroshan390@gmail.com>
2022-10-16 14:50:22 +0530

    Nav bar and main page is created (html,css,js)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+
+    $('#creates').hide()
+    $('#creates').fadeIn(330)
+
+    $('.menu').hide()
+    $('.menu').fadeIn(330)
+
+    $('#text2').hide()
+    $('#text2').fadeIn(330)
+
+    $('#btn').hide()
+    $('#btn').fadeIn(330)
+
+    $('#text3').hide()
+    $('#text3').fadeIn(330)
+
+    $('#text4').hide()
+    $('#text4').fadeIn(330)
+
+})
+
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
